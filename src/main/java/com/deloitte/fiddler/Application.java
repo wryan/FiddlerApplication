0f8507fbdebe61c2ac826214474a1d499c29ec2e
bmalouf@deloitte.com <bmalouf@deloitte.com>
2019-01-17 18:14:10 -0500

    adding kube dependency

--- src/main/java/com/deloitte/fiddler/Application.java
+++ src/main/java/com/deloitte/fiddler/Application.java
@@ -3,8 +3,10 @@ package com.deloitte.fiddler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
