e3deb0b9042c51b7b17d3bddc5ce902fe0b83a69
bmalouf@deloitte.com <bmalouf@deloitte.com>
2019-01-21 12:54:54 -0500

    adding new endpoint

--- src/main/java/com/deloitte/fiddler/controller/ProjectController.java
+++ src/main/java/com/deloitte/fiddler/controller/ProjectController.java
@@ -12,37 +12,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deloitte.fiddler.model.FiddlerProject;
+import com.deloitte.fiddler.model.FiddlerTask;
 import com.deloitte.fiddler.model.ProjectInitializer;
 import com.deloitte.fiddler.service.ProjectService;
-
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
-	
+
 	@Autowired
 	ProjectService ps;
-	
+
 	@PostMapping
-    public FiddlerProject createNewProject(@RequestBody ProjectInitializer pj) {
-        return this.ps.createProject(pj.getJsonURL());
-    }
-	
+	public FiddlerProject createNewProject(@RequestBody ProjectInitializer pj) {
+		return this.ps.createProject(pj.getJsonURL());
+	}
+
 	@GetMapping
 	public List<FiddlerProject> getProjects() {
 		return this.ps.getAllProjects();
-		
+
 	}
-	
+
 	@GetMapping("/{id}")
 	public FiddlerProject getProjectById(@PathVariable String id) {
 		return this.ps.getProjectByID(id);
-		
+
 	}
-	
+
 	@PutMapping("/{id}")
 	public FiddlerProject updateProject(@RequestBody FiddlerProject fp) {
 		return this.ps.updateProject(fp);
+
+	}
+
+	@PostMapping("/{id}/{procId}/{taskId}")
+	public FiddlerTask updateTask(@PathVariable String id, @PathVariable int procId, 
+			@PathVariable int taskId, @RequestBody FiddlerTask ft) {
+		return this.ps.updateTask(ft, id, procId, taskId);
 		
 	}
 }
