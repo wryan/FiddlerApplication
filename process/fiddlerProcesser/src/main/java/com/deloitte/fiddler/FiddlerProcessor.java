d04c109406638fe377966ac94055106879034e99
Bob Malouf <bmalouf@deloitte.com>
2019-02-24 08:52:49 -0500

    j

--- process/fiddlerProcesser/src/main/java/com/deloitte/fiddler/FiddlerProcessor.java
+++ process/fiddlerProcesser/src/main/java/com/deloitte/fiddler/FiddlerProcessor.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class FiddlerProcesser {
+public class FiddlerProcessor {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FiddlerProcesser.class, args);
+		SpringApplication.run(FiddlerProcessor.class, args);
 	}
 }
