4df2e6513ad8b20b7d6052c5d36cf9eab57f0bd8
Bob Malouf <bmalouf@deloitte.com>
2019-03-12 20:41:49 -0400

    f

--- service/fileService/src/main/java/com/deloitte/fiddler/FileService.java
+++ service/fileService/src/main/java/com/deloitte/fiddler/FileService.java
@@ -2,10 +2,10 @@ package com.deloitte.fiddler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class FileService {
 	public static void main(String[] args) {
 		SpringApplication.run(FileService.class, args);
